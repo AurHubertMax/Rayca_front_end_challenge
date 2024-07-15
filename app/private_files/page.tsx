900b69092e7ce4201aa953fa7fb2c5bbbac620a8
Aureliano Hubert Maximus <112564421+AurHubertMax@users.noreply.github.com>
2024-07-15 16:47:48 -0500

    Update page.tsx

--- app/private_files/page.tsx
+++ app/private_files/page.tsx
@@ -1,8 +1,12 @@
+import React, { Suspense } from 'react';
+
 export default function PrivateFiles() {
     return (
-      <div>
-        private files
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>
+          private files
+        </div>
+      </Suspense>
     );
 }
   
