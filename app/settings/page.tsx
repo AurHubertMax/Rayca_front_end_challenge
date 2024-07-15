0f7fd46264ac0db716a9daf9bd07144aa7763eac
Aureliano Hubert Maximus <112564421+AurHubertMax@users.noreply.github.com>
2024-07-15 16:48:27 -0500

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,26 +1,29 @@
 'use client'
 
+import React, { Suspense } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Security from "./tabs/Security/Security";
 
 
 export default function Settings() {
     return (
-      <Tabs defaultValue="Security" className="w-full" >
-        <TabsList>
-          <TabsTrigger value="general" >General</TabsTrigger>
-          <TabsTrigger value="Security">Security</TabsTrigger>
-          <TabsTrigger value="Billing">Billing</TabsTrigger>
-          <TabsTrigger value="Notifications">Notifications</TabsTrigger>
-          <TabsTrigger value="Apps">Apps</TabsTrigger>
-          <TabsTrigger value="Branding">Branding</TabsTrigger>
-          <TabsTrigger value="Refer a friend">Refer a friend</TabsTrigger>
-          <TabsTrigger value="Sharing">Sharing</TabsTrigger>
-        </TabsList>
-        <TabsContent value="Security">
-          <Security />
-        </TabsContent>
-      </Tabs>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Tabs defaultValue="Security" className="w-full" >
+          <TabsList>
+            <TabsTrigger value="general" >General</TabsTrigger>
+            <TabsTrigger value="Security">Security</TabsTrigger>
+            <TabsTrigger value="Billing">Billing</TabsTrigger>
+            <TabsTrigger value="Notifications">Notifications</TabsTrigger>
+            <TabsTrigger value="Apps">Apps</TabsTrigger>
+            <TabsTrigger value="Branding">Branding</TabsTrigger>
+            <TabsTrigger value="Refer a friend">Refer a friend</TabsTrigger>
+            <TabsTrigger value="Sharing">Sharing</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Security">
+            <Security />
+          </TabsContent>
+        </Tabs>
+      </Suspense>
     );
 }
   
