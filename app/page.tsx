ad54a40c5ca5b5baa0ea8f0ed2138f2eae7a65fa
Aureliano Hubert Maximus <112564421+AurHubertMax@users.noreply.github.com>
2024-07-15 16:49:44 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import React, { Suspense } from 'react';
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="outline">HomePage</Button>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Button variant="outline">HomePage</Button>
+      </div>
+    </Suspense>
   );
 }
